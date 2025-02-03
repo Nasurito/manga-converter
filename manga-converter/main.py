9588a311986f9ea93481d6aa8290165c00d9afc1
Nasurito <nathan.gering@gmail.com>
2025-02-03 01:32:38 +0100

    Style : Ajout d'une ligne de teste suplémentaire pour le deuxieme site

--- manga-converter/main.py
+++ manga-converter/main.py
@@ -2,3 +2,4 @@ from module.manga import Manga
 
 if __name__ == "__main__":
     test = Manga("https://mangakatana.com/manga/kaiju-no-8.24869")
+    test = Manga("https://www.lelmanga.com/manga/jujutsu-kaisen")
