8efc5d258532816bac43a9e36409c5787aa77e65
Nathan-grng <nathan.grng@eduge.ch>
2025-02-04 03:12:24 +0100

    Improuvement : Amélioration de la structure de la classe chapter

--- manga-converter/module/chapter.py
+++ manga-converter/module/chapter.py
@@ -2,13 +2,21 @@ import utils
 
 class Chapter:
     """Cette classe est utilisé pour définir un chapitre dans un manga, un chapitre posséde plusieurs pages (image récupérée depuis le site)"""
-    def __init__(self,link):
-        """Cette fonction est appalée à chaque création d'un chapitre, elle permet de récupéré les informations en fonctions du lien et des sites supporté
+    def __init__(self,id,link):
+        """
+        Cette fonction est appalée à chaque création d'un chapitre, elle permet de récupéré les informations en fonctions du lien et des sites supporté
 
         Args:
             link (string): lien du chapitre sur le site de scan
         """
-        chapter_html_page = utils.get_page(link).text
-        domain_name = utils.get_domain(link)
-        
-        
+        self.id_chapter = id
+        self.chapter_html_page = None
+        self.chapter_html_link = link
+        self.domain_name = utils.get_domain(link)
+        self.pages = []
+    
+    def id(self):
+        return float(self.id_chapter)    
+    
+    def get_scanned_pages(self):
+        self.chapter_html_page = utils.get_page(self.chapter_html_link)
