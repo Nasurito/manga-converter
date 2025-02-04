db385396b5f3a83ffeed86b9c5c99cab60e0ea5c
Nathan-grng <nathan.grng@eduge.ch>
2025-02-04 22:23:12 +0100

    Style : modification des batterie de test

--- manga-converter/main.py
+++ manga-converter/main.py
@@ -3,10 +3,7 @@ from module.manga import Manga
 if __name__ == "__main__":
     test = Manga("https://mangakatana.com/manga/kaiju-no-8.24869")
     test.review()
-    test.download_chapter(12)
-    test.download_chapters(12,24)
-    
     
     test = Manga("https://www.lelmanga.com/manga/jujutsu-kaisen")
     test.review()
-    test.download_chapter(12)
+    test.download_chapters(1,271.5)
