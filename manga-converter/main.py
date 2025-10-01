5a30234a294c531468386e9e1da9d05484661acd
nathan.gering <nathan.gering@hes-so.ch>
2025-10-01 11:18:04 +0200

    Style : Ajout de l'exemple

--- manga-converter/main.py
+++ manga-converter/main.py
@@ -3,11 +3,16 @@ from module.search_engine import Search_engine
 from module.manga import Manga
 
 if __name__ == "__main__":
+   
+    """
     search_input = input("🔍 Quel manga recherchez-vous ? ")
     engine = Search_engine(search_input)
+    if engine != None:
+        engine.Select_Manga()
+    """
+    
 
-
-    #test = Manga("https://www.lelmanga.com/manga/one-piece")
-    #test.review()
-    #test.download_chapter_to_epub(1, 200)
-    ##test.download_chapters(1,50,"CBR")
+    test = Manga("https://www.lelmanga.com/manga/shuumatsu-no-valkyrie")
+    test.review()
+    test.download_chapter_to_epub(1, 5)
+    #test.download_chapters(1,50,"CBR")
